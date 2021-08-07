dcb26de8a055474ebcf4a7ac46c0122f2cb93cbf
Axel Paez <apaezb@miumg.edu.gt>
2021-08-07 15:20:04 -0500

    config.example agregado

---
+++ config/config.example.js
@@ -0,0 +1,14 @@
+class Config {
+
+    constructor(){
+            this.port = 0000;
+            this.database = {
+            host: 'host',
+            user: 'user',
+            password: 'password',
+            database: 'DB name'
+        };
+    }
+}
+
+module.exports = Config;
